ec61ec6bcd9995fa3b5036c5f04ae1e7034ad209
Agustin-Perezz <agustinnicolas806@gmail.com>
2022-11-29 12:12:09 -0300

    Add base url in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+    base: '/gha-test/',
     plugins: [react()],
     test: {
         globals: true,
