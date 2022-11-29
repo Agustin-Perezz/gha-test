ddccd0bc961320e27d05e0ca1273bbf289f4d4b5
Agustin-Perezz <agustinnicolas806@gmail.com>
2022-11-29 16:48:51 -0300

    Add new elements at the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 function App() {
     return (
         <div className="App">
-            <h2>test</h2>
-            <h3>coscu</h3>
+            <h2>This is a simple template</h2>
+            <p>GitHub Actions</p>
         </div>
     );
 }
